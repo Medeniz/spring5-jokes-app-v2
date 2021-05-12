57420b30eed07cc731339f60436d1ff78c3b0e1b
medeniz <medeniz@gmail.com>
2021-05-12 20:30:00 +0200

    Create Tamplate closes #5

--- src/main/java/be/intecbrussel/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/be/intecbrussel/spring5jokesappv2/controllers/JokeController.java
@@ -1,7 +1,6 @@
 package be.intecbrussel.spring5jokesappv2.controllers;
 
 import be.intecbrussel.spring5jokesappv2.services.JokeService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class JokeController {
     private final JokeService jokeService;
 
-   // @Autowired
+    // @Autowired ---Bir tane oldugu icin geek yok
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping({"",""})
-    public String showJoke(Model model){
-        model.addAttribute("joke" , jokeService.getJoke());
+    @RequestMapping({"", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
 
         return "index";
 
